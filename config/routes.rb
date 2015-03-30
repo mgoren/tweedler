cfb1619b2775569d255d9a901ff1b6634a95b423
Mike Goren and Tina Ramsey <tina.ramsey87@gmail.com>
2015-03-30 10:33:17 -0700

    add tweeds to routes and db

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "users#index"
+  root to: "tweeds#index"
 
   resources :users
+
+  resources :tweeds
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330164602) do
+ActiveRecord::Schema.define(version: 20150330173144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweeds", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
